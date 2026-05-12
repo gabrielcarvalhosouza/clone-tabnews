25b433836587c3a48edf9da85d3bcf2527782dc8
Gabriel Carvalho Souza <gcarvalho2112@gmail.com>
2026-05-12 17:51:11 -0300

    feat: add support for `PATCH` on `/api/v1/users/[username]`

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -86,7 +86,7 @@ describe("POST /api/v1/users", () => {
       expect(response2Body).toEqual({
         name: "ValidationError",
         message: "O email ou username informado já está em uso.",
-        action: "Utilize outro email ou username para continuar o cadastro.",
+        action: "Utilize outro email ou username para realizar esta operação.",
         status_code: 400,
       });
     });
@@ -123,7 +123,7 @@ describe("POST /api/v1/users", () => {
       expect(response2Body).toEqual({
         name: "ValidationError",
         message: "O email ou username informado já está em uso.",
-        action: "Utilize outro email ou username para continuar o cadastro.",
+        action: "Utilize outro email ou username para realizar esta operação.",
         status_code: 400,
       });
     });
