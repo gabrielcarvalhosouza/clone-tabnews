1e987ea1d3fcb593e229cd04b4a13fbd24701a5b
Gabriel Carvalho Souza <gcarvalho2112@gmail.com>
2026-05-21 18:21:49 -0300

    feat: add `activation.findOneValidById()` and `orchestrator.extractUUID()`

--- models/activation.js
+++ models/activation.js
@@ -1,14 +1,15 @@
 import email from "infra/email.js";
 import database from "infra/database.js";
 import webserver from "infra/webserver.js";
+import { NotFoundError } from "infra/errors.js";
 
 const EXPIRATION_IN_MILLISECONDS = 60 * 15 * 1000;
 
-async function findOneByUserId(userId) {
-  const newToken = await runSelectQuery(userId);
-  return newToken;
+async function findOneValidById(tokenId) {
+  const activationTokenObject = await runSelectQuery(tokenId);
+  return activationTokenObject;
 
-  async function runSelectQuery(userId) {
+  async function runSelectQuery(tokenId) {
     const results = await database.query({
       text: `
       SELECT 
@@ -16,12 +17,22 @@ async function findOneByUserId(userId) {
       FROM 
         user_activation_tokens
       WHERE 
-        user_id = $1
+        id = $1
+        AND expires_at > now()
+        AND used_at IS NULL
       LIMIT
         1
       ;`,
-      values: [userId],
+      values: [tokenId],
     });
+
+    if (results.rowCount === 0) {
+      throw new NotFoundError({
+        message:
+          "O token de ativação utilizado nao foi encontrado no sistema ou expirou.",
+        action: "Faça um novo cadastro.",
+      });
+    }
 
     return results.rows[0];
   }
@@ -66,7 +77,7 @@ Gcarvalho
 }
 
 const activation = {
-  findOneByUserId,
+  findOneValidById,
   create,
   sendEmailToUser,
 };
