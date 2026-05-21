482612af24b76fae3ab58d72e022433690fdf42c
Gabriel Carvalho Souza <gcarvalho2112@gmail.com>
2026-05-21 15:34:36 -0300

    feat: add `features` collum to `users` table

--- tests/integration/api/v1/users/[username]/patch.test.js
+++ tests/integration/api/v1/users/[username]/patch.test.js
@@ -119,6 +119,7 @@ describe("PATCH /api/v1/users/[username]", () => {
         username: "uniqueUser2",
         email: responseBody.email,
         password: responseBody.password,
+        features: [],
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
       });
@@ -156,6 +157,7 @@ describe("PATCH /api/v1/users/[username]", () => {
         username: responseBody.username,
         email: "uniqueEmail2@gmail.com",
         password: responseBody.password,
+        features: [],
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
       });
@@ -193,6 +195,7 @@ describe("PATCH /api/v1/users/[username]", () => {
         username: responseBody.username,
         email: responseBody.email,
         password: responseBody.password,
+        features: [],
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
       });
